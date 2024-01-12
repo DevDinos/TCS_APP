f417168a17620e8f5e83f5fc59e30bda4d8e5c76
Catalina <arreolc2@uci.edu>
2024-01-11 18:17:23 -0800

    Calendar Selection
    Changed the code to be able to store in a variable the current selected date in the calendar.

--- 191A_App/src/app/Calendar/Calendar.page.ts
+++ 191A_App/src/app/Calendar/Calendar.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 //import { NavController } from 'ionic-angular';
+import { CalendarComponent, Step } from 'ionic7-calendar'; //Added for the Calendar
+import { CalendarMode } from 'ionic7-calendar';
 
 @Component({
   selector: 'app-Calendar',
@@ -8,8 +10,19 @@ import { Router } from '@angular/router';
   styleUrls: ['Calendar.page.scss']
 })
 export class CalendarPage {
+  allEvents = [];
 
-  constructor(private route: Router){
+  selectedDate: string = '';
+
+  constructor(private route: Router) { 
   }
 
+  onDateSelectionChange(event: any) {
+    console.log('Selected Date:', this.selectedDate);
+  }
+
+
+
 }
+
+//New Class for the Calendar
