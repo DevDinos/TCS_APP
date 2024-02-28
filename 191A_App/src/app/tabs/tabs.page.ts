f81d5f90b1c8b7730985e285bb1dfaa6a1ad1bee
Brendan Grant <1437condoor@gmail.com>
2024-02-27 16:44:11 -0800

    Current Progress on implementing navBar.

--- 191A_App/src/app/tabs/tabs.page.ts
+++ 191A_App/src/app/tabs/tabs.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
+
+//Source:
+// https://stackoverflow.com/questions/74651790/how-to-update-the-variable-value-in-the-component-in-angular
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +11,22 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private route: Router, private aRoute: ActivatedRoute) {}
 
+  currentPage: String = "None";
+
+  ngOnInit(){
+    console.log(this.route.url);
+  }
+
+  navigateTo(destination:any){
+    let validDestinations: Array<string> = ["HomePage", "Forum", "Resources", "Calendar"];
+
+    if (validDestinations.includes(destination)){
+      let tempDestination: string = "/tabs/" + destination;
+      this.route.navigate([tempDestination]);
+      this.currentPage = destination;
+    }
+  }
+  
 }
