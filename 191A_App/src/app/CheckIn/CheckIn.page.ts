58f65f4ae8ae7d130ae60f2d9f592c2153301dfd
wloii <wloi@uci.edu>
2024-02-28 18:11:10 -0800

    added modalService for popup, checkin functions

--- 191A_App/src/app/CheckIn/CheckIn.page.ts
+++ 191A_App/src/app/CheckIn/CheckIn.page.ts
@@ -13,45 +13,93 @@ export class CheckInPage {
   showPart1: boolean = true;
   name: string = '';
   selectedMood: string = '';
+  moodTrue: boolean = false;
+  skipTrue: boolean = false;
 
   constructor(private route: Router, private modalCtrl: ModalController){
   }
+
+  ngOnInit(){
+    this.skipTrue = false;
+  }
   
   async showPart2() {
+    if(this.skipTrue === true){
+      await this.modalCtrl.dismiss();
+    
+      const modal = await this.modalCtrl.create({
+        component: CheckIn2Page,
+        cssClass: 'checkin-size',
+        componentProps:{
+          selectedMood: this.selectedMood,
+          submitDate: new Date()
+        }
+      });
+
+      modal.onWillDismiss().then((result) => {
+        if (result.role === 'complete') {
+          console.log('Modal complete')
+          this.modalCtrl.dismiss()
+        } else if(result.role === 'skip'){
+          console.log('Modal Skipped')
+          this.modalCtrl.dismiss();
+        }
+      });
+
+      await modal.present();
+      this.submitTime();
+    }
+
+    if(!this.moodTrue){
+      console.log('no mood')
+      return;
+    } 
+
+    console.log('submitted')
     await this.modalCtrl.dismiss();
     
     const modal = await this.modalCtrl.create({
       component: CheckIn2Page,
       cssClass: 'checkin-size',
       componentProps:{
-        selectedMood: this.selectedMood
+        selectedMood: this.selectedMood,
+        submitDate: new Date()
       }
     });
-
+  
     modal.onWillDismiss().then((result) => {
       if (result.role === 'complete') {
-        // Handle completion logic if needed
         console.log('Modal complete')
         this.modalCtrl.dismiss()
       } else if(result.role === 'skip'){
-        // Handle other actions if needed
         console.log('Modal Skipped')
         this.modalCtrl.dismiss();
       }
     });
-
+  
     await modal.present();
-  }
-
-  cancel() {
-    return this.modalCtrl.dismiss(null, 'cancel');
-  }
-
-  confirm() {
-    return this.modalCtrl.dismiss(this.name, 'confirm');
+    this.submitTime();
   }
 
   selectMood(mood:string){
-    this.selectedMood = mood;
+    if(this.selectedMood === mood){
+      this.selectedMood = '';
+      this.moodTrue = false;
+    } else{
+      this.selectedMood = mood;
+      this.moodTrue = true;
+    }
+  }
+
+  skipMood(){
+    console.log(this.skipTrue)
+    console.log('skipped')
+    this.skipTrue = true;
+    this.showPart2();
+  }
+
+  submitTime(){
+    const submitDate = new Date();
+    console.log('Current Date & Time is: ', submitDate, ', and mood selected is: ', this.selectedMood);
   }
 }
--- 191A_App/src/app/HomePage/HomePage.page.ts
+++ 191A_App/src/app/HomePage/HomePage.page.ts
@@ -2,31 +2,49 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { CheckInPage } from '../CheckIn/CheckIn.page';
+import {ModalService} from '../services/modal.service';
 @Component({
   selector: 'app-HomePage',
   templateUrl: 'HomePage.page.html',
   styleUrls: ['HomePage.page.scss']
 })
+
 export class HomePagePage {
-  message = ""
+  checkinButton: boolean = false;
 
-  constructor(private route: Router, private modalCtrl: ModalController){
-    console.log('HomePagePage constructor');
+  constructor(private route: Router, private modalCtrl: ModalController, private modalService: ModalService){
+    console.log('HomePagePage constructor, flag: ', this.modalService.modalAlreadyShown);
   }
-  async ionViewDidEnter() {
-    console.log('Starting to create modal');
-    const modal = await this.modalCtrl.create({
-      component: CheckInPage,   
-      cssClass: 'checkin-size',
-    });
-    modal.present();
-    console.log('Modal Created: ', modal);
-    const { data, role } = await modal.onWillDismiss();
 
-    if (role === 'confirm') {
-      this.message = `Hello, ${data}!`;
-    } else{
-      this.message = '';
+  async ionViewDidEnter() {
+    console.log('Starting to create modal, checkin button: ', this.checkinButton);
+    if(this.checkinButton){
+      console.log('checkin click')
+      const modal = await this.modalCtrl.create({
+        component: CheckInPage,   
+        cssClass: 'checkin-size',
+      });
+      modal.present();
+      console.log('Modal Created: ', modal);
+      const { data, role } = await modal.onWillDismiss();
+  
+    } else if(!this.modalService.modalAlreadyShown){
+      console.log('popup')
+      const modal = await this.modalCtrl.create({
+        component: CheckInPage,   
+        cssClass: 'checkin-size',
+      });
+      modal.present();
+      console.log('Modal Created: ', modal);
+      const { data, role } = await modal.onWillDismiss();
+      
+      this.modalService.modalAlreadyShown = true;
     }
+  }
+
+  checkinClick(){
+    console.log(this.modalService.modalAlreadyShown)
+    this.checkinButton = true;
+    this.ionViewDidEnter();
   }
 }
