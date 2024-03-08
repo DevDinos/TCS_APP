fcf5af4f60a3cd0c6fe2074d22f110070c088e00
Brendan Grant <1437condoor@gmail.com>
2024-03-08 10:06:48 -0800

    Updated Welcome with mysql program from link

--- 191A_App/src/app/Welcome/Welcome.page.ts
+++ 191A_App/src/app/Welcome/Welcome.page.ts
@@ -31,4 +31,208 @@ export class WelcomePage {
 
   }
 
+
+//Source: https://learn.microsoft.com/en-us/azure/mysql/flexible-server/connect-nodejs?tabs=windows
+
+/* Create Program
+const mysql = require('mysql2');
+const fs = require('fs');
+
+var config =
+{
+    host: 'your_server_name.mysql.database.azure.com',
+    user: 'your_admin_name',
+    password: 'your_admin_password',
+    database: 'quickstartdb',
+    port: 3306,
+    ssl: {ca: fs.readFileSync("your_path_to_ca_cert_file_DigiCertGlobalRootCA.crt.pem")}
+};
+
+const conn = new mysql.createConnection(config);
+
+conn.connect(
+    function (err) {
+    if (err) {
+        console.log("!!! Cannot connect !!! Error:");
+        throw err;
+    }
+    else
+    {
+        console.log("Connection established.");
+        queryDatabase();
+    }
+});
+
+function queryDatabase()
+{
+    conn.query('DROP TABLE IF EXISTS inventory;',
+        function (err, results, fields) {
+            if (err) throw err;
+            console.log('Dropped inventory table if existed.');
+        }
+    )
+    conn.query('CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);',
+        function (err, results, fields) {
+            if (err) throw err;
+            console.log('Created inventory table.');
+        }
+    )
+    conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['banana', 150],
+        function (err, results, fields) {
+            if (err) throw err;
+            else console.log('Inserted ' + results.affectedRows + ' row(s).');
+        }
+    )
+    conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['orange', 250],
+        function (err, results, fields) {
+            if (err) throw err;
+            console.log('Inserted ' + results.affectedRows + ' row(s).');
+        }
+    )
+    conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['apple', 100],
+        function (err, results, fields) {
+            if (err) throw err;
+            console.log('Inserted ' + results.affectedRows + ' row(s).');
+        }
+    )
+    conn.end(function (err) {
+        if (err) throw err;
+        else  console.log('Done.')
+    });
+};
+*/
+
+/* Read Program
+const mysql = require('mysql2');
+const fs = require('fs');
+
+var config =
+{
+    host: 'your_server_name.mysql.database.azure.com',
+    user: 'your_admin_name',
+    password: 'your_admin_password',
+    database: 'quickstartdb',
+    port: 3306,
+    ssl: {ca: fs.readFileSync("your_path_to_ca_cert_file_DigiCertGlobalRootCA.crt.pem")}
+};
+
+const conn = new mysql.createConnection(config);
+
+conn.connect(
+    function (err) {
+        if (err) {
+            console.log("!!! Cannot connect !!! Error:");
+            throw err;
+        }
+        else {
+            console.log("Connection established.");
+            readData();
+        }
+    });
+
+function readData(){
+    conn.query('SELECT * FROM inventory',
+        function (err, results, fields) {
+            if (err) throw err;
+            else console.log('Selected ' + results.length + ' row(s).');
+            for (i = 0; i < results.length; i++) {
+                console.log('Row: ' + JSON.stringify(results[i]));
+            }
+            console.log('Done.');
+        })
+    conn.end(
+        function (err) {
+            if (err) throw err;
+            else  console.log('Closing connection.')
+    });
+};
+*/
+
+/* Update Program
+const mysql = require('mysql2');
+const fs = require('fs');
+
+var config =
+{
+    host: 'your_server_name.mysql.database.azure.com',
+    user: 'your_admin_name',
+    password: 'your_admin_password',
+    database: 'quickstartdb',
+    port: 3306,
+    ssl: {ca: fs.readFileSync("your_path_to_ca_cert_file_DigiCertGlobalRootCA.crt.pem")}
+};
+
+const conn = new mysql.createConnection(config);
+
+conn.connect(
+    function (err) {
+        if (err) {
+            console.log("!!! Cannot connect !!! Error:");
+            throw err;
+        }
+        else {
+            console.log("Connection established.");
+            updateData();
+        }
+    });
+
+function updateData(){
+       conn.query('UPDATE inventory SET quantity = ? WHERE name = ?', [75, 'banana'],
+            function (err, results, fields) {
+                if (err) throw err;
+                else console.log('Updated ' + results.affectedRows + ' row(s).');
+           })
+       conn.end(
+           function (err) {
+                if (err) throw err;
+                else  console.log('Done.')
+        });
+};
+*/
+
+/* Delete Program
+
+const mysql = require('mysql2');
+const fs = require('fs');
+
+var config =
+{
+    host: 'your_server_name.mysql.database.azure.com',
+    user: 'your_admin_name',
+    password: 'your_admin_password',
+    database: 'quickstartdb',
+    port: 3306,
+    ssl: {ca: fs.readFileSync("your_path_to_ca_cert_file_DigiCertGlobalRootCA.crt.pem")}
+};
+
+const conn = new mysql.createConnection(config);
+
+conn.connect(
+    function (err) {
+        if (err) {
+            console.log("!!! Cannot connect !!! Error:");
+            throw err;
+        }
+        else {
+            console.log("Connection established.");
+            deleteData();
+        }
+    });
+
+function deleteData(){
+       conn.query('DELETE FROM inventory WHERE name = ?', ['orange'],
+            function (err, results, fields) {
+                if (err) throw err;
+                else console.log('Deleted ' + results.affectedRows + ' row(s).');
+           })
+       conn.end(
+           function (err) {
+                if (err) throw err;
+                else  console.log('Done.')
+        });
+};
+
+*/
+
+
 }
