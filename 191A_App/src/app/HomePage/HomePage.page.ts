5e06b0c58908f7f853832d8cd920ba44f6f984ed
Leslie Lei <93801278+ljlei@users.noreply.github.com>
2024-03-12 00:09:53 -0700

    updated styling on homepage
    - changed heading to Home vs. Sunrise Connections
    - added margins to top of back and helpline button
    - pushed helpline button to right edge of screen
    - moved welcome statement to center of screen
    - enlarged nav buttons
    - added zooming functionality
    - enlarged fonts using global variables

--- 191A_App/src/app/HomePage/HomePage.page.ts
+++ 191A_App/src/app/HomePage/HomePage.page.ts
@@ -14,6 +14,7 @@ import { NavigationService } from '../Services/navigation.service';
 export class HomePagePage {
   checkinButton: boolean = false;
 
+  zoomFactor = 1;
   constructor(private modalCtrl: ModalController, private modalService: ModalService, private tabsPage: TabsPage, private ns: NavigationService){
     console.log('HomePagePage constructor, flag: ', this.modalService.modalAlreadyShown);
   }
@@ -62,5 +63,17 @@ export class HomePagePage {
     this.checkinButton = true;
     this.ionViewDidEnter();
   }
+
+  // Method to increase font size
+zoomIn() {
+  this.zoomFactor += 0.1; // Increase zoom factor by 0.1
+  document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
+}
+
+// Method to decrease font size
+zoomOut() {
+  this.zoomFactor -= 0.1; // Decrease zoom factor by 0.1
+  document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
+}
 }
 
