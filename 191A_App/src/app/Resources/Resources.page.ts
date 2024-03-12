6a7c3be2dc0438b5260ccc110993ce3bd7a78bbd
terynk <86626692+terynk@users.noreply.github.com>
2024-03-11 17:01:09 -0700

    added zoom button to resources page

--- 191A_App/src/app/Resources/Resources.page.ts
+++ 191A_App/src/app/Resources/Resources.page.ts
@@ -12,6 +12,7 @@ export class ResourcesPage {
 
   constructor(private ns: NavigationService, private tabsPage: TabsPage){
   }
+  zoomFactor = 1;
 
   ngOnInit(){
     this.tabsPage.revealTab();
@@ -19,6 +20,18 @@ export class ResourcesPage {
 
   navigateTo(destination: string){
     this.ns.navigateTo(destination);
+  }
+
+  // Method to increase font size
+  zoomIn() {
+    this.zoomFactor += 0.1; // Increase zoom factor by 0.1
+    document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
+  }
+
+  // Method to decrease font size
+  zoomOut() {
+    this.zoomFactor -= 0.1; // Decrease zoom factor by 0.1
+    document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
   }
 
 }
