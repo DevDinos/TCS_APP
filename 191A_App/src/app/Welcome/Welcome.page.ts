d07f74ac417d71c963922b5b4d5e5e424bfc8a63
Leslie Lei <93801278+ljlei@users.noreply.github.com>
2024-03-14 16:23:45 -0700

    updated welcome page
    - added zoom-buttons to welcome page

--- 191A_App/src/app/Welcome/Welcome.page.ts
+++ 191A_App/src/app/Welcome/Welcome.page.ts
@@ -17,6 +17,8 @@ export class WelcomePage {
 
   public baseURL = "sunriseconnectionsserver.mysql.database.azure.com";
 
+  zoomFactor = 1;
+  
   constructor(private httpClient: HttpClient, private ns: NavigationService){
   }
 
@@ -75,6 +77,17 @@ export class WelcomePage {
         });
         };
         */
+         // Method to increase font size
+        zoomIn() {
+            this.zoomFactor += 0.1; // Increase zoom factor by 0.1
+            document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
+        }
+        
+        // Method to decrease font size
+        zoomOut() {
+            this.zoomFactor -= 0.1; // Decrease zoom factor by 0.1
+            document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
+        }
     }
 
     //Source: https://learn.microsoft.com/en-us/azure/mysql/flexible-server/connect-nodejs?tabs=windows
