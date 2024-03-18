4b944b4c7360213b6a5107e8f41ece4b1af4bd8f
Leslie Lei <93801278+ljlei@users.noreply.github.com>
2024-03-17 20:34:49 -0700

    updated calendar page and log out button
    - removed zoom button in calendar.page.html and calendar.page.scss and calendar.page.ts
    - adjusted tab labels font size using global variable
    - adjusted z-index of logOutButton

--- 191A_App/src/app/Calendar/Calendar.page.ts
+++ 191A_App/src/app/Calendar/Calendar.page.ts
@@ -161,17 +161,17 @@ export class CalendarPage {
 
   }
 
-  // Method to increase font size
-  zoomIn() {
-    this.zoomFactor += 0.1; // Increase zoom factor by 0.1
-    document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
-  }
+  // // Method to increase font size
+  // zoomIn() {
+  //   this.zoomFactor += 0.1; // Increase zoom factor by 0.1
+  //   document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
+  // }
 
-  // Method to decrease font size
-  zoomOut() {
-    this.zoomFactor -= 0.1; // Decrease zoom factor by 0.1
-    document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
-  }
+  // // Method to decrease font size
+  // zoomOut() {
+  //   this.zoomFactor -= 0.1; // Decrease zoom factor by 0.1
+  //   document.documentElement.style.setProperty('--zoom-factor', this.zoomFactor.toString()); // Update zoom factor in CSS
+  // }
 }
 
 //New Class for the Calendar
